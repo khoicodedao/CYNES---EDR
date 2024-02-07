ef5476ef329d4198230b292543b773a2f86375c8
Khoi <dangkhoi29mta@gmail.com>
2024-02-07 21:52:28 +0700

    [Add] terminal

--- components/ControlDirectly/index.tsx
+++ components/ControlDirectly/index.tsx
@@ -1,25 +1,34 @@
 "use client";
 import StatisticAgents from "./StatisticControl";
 import Terminal, { ColorMode, TerminalOutput } from "react-terminal-ui";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
 const ControlDirectly = () => {
   const [terminalLineData, setTerminalLineData] = useState([""]);
+  useEffect(() => {
+    const socket = io("http://localhost:8000");
+    socket.on("connect", () => {
+      console.log("Connected to Socket.IO server");
+    });
+    socket.emit("message", {
+      //reqire token, client (id) in header when connect
+      // "from":"user1",
+      to: "test",
+      command_type: "CMD",
+      command_info: {
+        dir: "c:\\windows\\",
+        cmd: "dir",
+      },
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <>
-      {/* <!-- ====== FunctionBar Section Start ====== --> */}
-      {/* <div className="w-full max-w-full rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-        <FunctionBar></FunctionBar>
-      </div> */}
-      {/* <!-- ====== FunctionBar Section End ====== --> */}
-
-      {/* <!-- ====== StatisticEvent Section Start ====== --> */}
       <div className=" mt-1 w-full max-w-full rounded-sm border border-stroke bg-white  dark:border-strokedark dark:bg-boxdark shadow-default">
         <StatisticAgents></StatisticAgents>
       </div>
-
-      {/* <!-- ====== Event Section End ====== --> */}
-      {/* <!-- ====== DataGrid Section Start ====== --> */}
-
       <div className=" mt-1 w-full max-w-full rounded-sm border border-stroke bg-white  dark:border-strokedark dark:bg-boxdark shadow-default">
         <Terminal
           height="200"
