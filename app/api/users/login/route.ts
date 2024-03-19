5f27985d8db7b681d8bba887b4f02913933e780e
Nguyễn Khôi <nguyenkhoi@MacBook-Air-cua-Nguyen.local>
2024-03-19 22:18:58 +0700

    [Fix] Ui design on layout

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -34,7 +34,7 @@ export async function POST(request: NextRequest) {
       success: true,
     });
     response.cookies.set("token", token, {
-      httpOnly: true,
+      httpOnly: false,
     });
     return response;
   } catch (error: any) {
