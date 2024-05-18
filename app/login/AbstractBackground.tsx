cee999414f63a5801257ff4b5defe850815fc89d
Khoi <dangkhoi29mta@gmail.com>
2024-05-18 22:45:46 +0700

    [ChangeUI] Login page

---
+++ app/login/AbstractBackground.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const AbstractBackground = () => {
+  return (
+    <>
+      <div className="abstract-background">
+        <img className="group-icon" alt="" src="/images/Login/group.svg" />
+        <img className="group-icon1" alt="" src="/images/Login/group1.svg" />
+        <div className="group">
+          <img className="group-icon2" alt="" src="/images/Login/group2.svg" />
+          <img className="group-icon3" alt="" src="/images/Login/group3.svg" />
+          <img className="group-icon4" alt="" src="/images/Login/group4.svg" />
+          <div className="group1">
+            <img
+              className="group-icon5"
+              alt=""
+              src="/images/Login/group5.svg"
+            />
+            <img
+              className="group-icon6"
+              alt=""
+              src="/images/Login/group6.svg"
+            />
+            <img
+              className="group-icon7"
+              alt=""
+              src="/images/Login/group7.svg"
+            />
+          </div>
+        </div>
+        <img className="group-icon8" alt="" src="/images/Login/group8.svg" />
+        <img className="group-icon9" alt="" src="/images/Login/group9.svg" />
+        <img className="group-icon10" alt="" src="/images/Login/group10.svg" />
+        <img className="group-icon11" alt="" src="/images/Login/group11.svg" />
+        <div className="group2">
+          <img
+            className="group-icon12"
+            alt=""
+            src="/images/Login/group12.svg"
+          />
+          <img
+            className="group-icon13"
+            alt=""
+            src="/images/Login/group13.svg"
+          />
+          <img
+            className="group-icon14"
+            alt=""
+            src="/images/Login/group14.svg"
+          />
+          <div className="group3">
+            <img
+              className="group-icon15"
+              alt=""
+              src="/images/Login/group15.svg"
+            />
+            <img
+              className="group-icon16"
+              alt=""
+              src="/images/Login/group16.svg"
+            />
+          </div>
+        </div>
+        <img className="group-icon17" alt="" src="/images/Login/group17.svg" />
+        <img className="group-icon18" alt="" src="/images/Login/group18.svg" />
+      </div>
+    </>
+  );
+};
+
+export default AbstractBackground;
