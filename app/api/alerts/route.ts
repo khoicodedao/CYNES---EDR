5f6c5eff50cf305971825777f458a3bfe5c40f4e
Khoi <dangkhoi29mta@gmail.com>
2024-05-29 15:11:28 +0700

    [Fix] pagination alert page

--- app/api/alerts/route.ts
+++ app/api/alerts/route.ts
@@ -14,6 +14,7 @@ export async function POST(request: NextRequest) {
       const response = NextResponse.json({
         success: true,
         alerts: res.alerts,
+        count: res.count,
       });
       return response;
     } else return "Error";
